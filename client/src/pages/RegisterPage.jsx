870c5ba017d11d866ae227221f95d03e6e35580c
Veronica Maya Santi <veronicamayasanti@gmail.com>
2025-06-15 15:32:28 +0700

    register client + css

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { registerUser } from '../api/userService.js'; // Pastikan untuk mengimpor fungsi registerUser
+import { registerUser } from '../api/userService.js'; 
 
 const RegisterPage = () => {
     // State untuk menyimpan data input
@@ -7,7 +7,7 @@ const RegisterPage = () => {
     const [phoneNumber, setPhoneNumber] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [roleId, setRoleId] = useState(3);
+    const [roleId] = useState(3);
     const [gradeId, setGradeId] = useState(1); // Atau nilai default sesuai kebutuhan
     const [error, setError] = useState(null);
     const [successMessage, setSuccessMessage] = useState(null);
@@ -17,7 +17,7 @@ const RegisterPage = () => {
         e.preventDefault(); // Mencegah reload halaman saat submit form
 
         try {
-            const userData = { name, phone_number: phoneNumber, email, password, grade_id: gradeId };
+            const userData = { name, phone_number: phoneNumber, email, password,role_id: roleId, grade_id: gradeId };
             const response = await registerUser(userData);
 
             if (response.error) {
@@ -34,72 +34,122 @@ const RegisterPage = () => {
             setSuccessMessage(null);
         }
     };
-
     return (
-        <div className="register-user">
-            <h2>Register User</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Name:</label>
-                    <input
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Phone Number:</label>
-                    <input
-                        type="text"
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNumber(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                    Register Account
+                </h2>
+            </div>
+                <p className="mt-10 text-center text-xl/9 font-normal tracking-tight text-red-600">
+                    {error && <p className="error">{error}</p>}
+                    {successMessage && <p className="success">{successMessage}</p>}
+                </p>
 
-                <div>
-                    <label>Role ID:</label>
-                    <input
-                        type="number"
-                        value={roleId}
-                        onChange={(e) => setGradeId(e.target.value)}
-                        required
-                    />
-                </div>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form onSubmit={handleSubmit} className="space-y-6">
 
-                <div>
-                    <label>Grade ID:</label>
-                    <input
-                        type="number"
-                        value={gradeId}
-                        onChange={(e) => setGradeId(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit">Register</button>
-            </form>
+                    <div>
+                        <label htmlFor="name" className="block text-sm/6 font-medium text-gray-900">
+                            Your Name
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                required
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
 
-            {error && <p className="error">{error}</p>}
-            {successMessage && <p className="success">{successMessage}</p>}
+
+                    <div>
+                        <label htmlFor="name" className="block text-sm/6 font-medium text-gray-900">
+                            Phone Number
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                type="text"
+                                value={phoneNumber}
+                                onChange={(e) => setPhoneNumber(e.target.value)}
+                                required
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                            Email address
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                                autoComplete="email"
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                                Password
+                            </label>
+                            <div className="text-sm">
+                                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                    Forgot password?
+                                </a>
+                            </div>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                                autoComplete="current-password"
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+
+                    {/* Hidden input for role ID */}
+                    <input type="hidden" value={roleId} />
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                                Grade ID :
+                            </label>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                type="number"
+                                value={gradeId}
+                                onChange={(e) => setGradeId(e.target.value)}
+                                required
+                                autoComplete="current-password"
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Register
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
